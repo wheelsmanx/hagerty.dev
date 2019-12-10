46907e4b308f7e2f37a9f2dd25b2d2c6eb3e3ba8
wheelsmanx <wheelsmanx@gmail.com>
2019-12-10 04:54:58 -0500

    update templates
    changed templates to ensure they worked moving forward

--- hagerty_dev/__init__.py
+++ hagerty_dev/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+
+from datetime import datetime
+from flask import render_template
+
+
 @app.route("/")
 def hello():
-    return "Under Construction"
+    return render_template('index.html')
+	
+	
+	
 if __name__ == "__main__":
     app.run('0.0.0.0')
