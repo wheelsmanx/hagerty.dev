9d228726ce4fe7ca8f8adc6dd462b3fda94fd060
Justin Hagerty <justin.hagerty@dynatrace.com>
2019-12-13 19:03:20 -0500

    init templates and boostrap framework
    bootstrap framework added --- add some base info --- needs revision

--- hagerty_dev/__init__.py
+++ hagerty_dev/__init__.py
@@ -6,10 +6,28 @@ from flask import render_template
 
 
 @app.route("/")
-def hello():
+def home():
+    return render_template('index.html')
+
+@app.route("/contactMe")
+def contactMe():
     return render_template('index.html')
 	
+@app.route("/slackBots")
+def slackBots():
+    return render_template('index.html')
 	
+@app.route("/test")
+def test_home():
+    return render_template('/test/index.html')
+
+@app.route("/test/contactMe")
+def test_contactMe():
+    return render_template('/test/contactMe.html')
+	
+@app.route("/test/slackBots")
+def test_slackBots():
+    return render_template('/test/slackBots.html')
 	
 if __name__ == "__main__":
     app.run('0.0.0.0')
